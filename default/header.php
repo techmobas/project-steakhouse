59854e05938d2b417bbd41ca071e9ba8b97e5d84
Kumagawa15 <ikhsangojali@gmail.com>
2022-06-10 00:12:12 +0700

    Pengeditan Menu

--- default/header.php
+++ default/header.php
@@ -9,6 +9,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.css">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
