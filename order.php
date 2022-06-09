6d2c0114624ef3103d5f068a32f2cf0306bccd68
techmobas <techmobabas@gmail.com>
2022-06-10 05:10:44 +0700

    fix order

--- order.php
+++ order.php
@@ -4,6 +4,17 @@
         header('location: login.php?error=nologin');
         exit();  
     }   
+
+    if(isset($_SESSION['order'])){
+        $email = $_POST["email"];
+        $fullname = $_POST["fullname"];
+        $password = $_POST["password"];
+        $password_repeat = $_POST["repeat_password"];
+        $phone = $_POST["phone"];
+
+        require_once 'config.php';
+    }
+
 ?>
 
 <style>
@@ -55,11 +66,11 @@ border-bottom-right-radius: 16px;
                   <div class="d-flex justify-content-between align-items-center mb-5">
                     <h1 class="fw-bold mb-0 text-black">Daftar Makanan</h1>
                   </div>
-                <form>
+                <form action="order.php" method="post">
                 <hr class="my-4">
-                <h3>Beef Steak</h3>
+
                     <?php
-                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id=12");
+                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id ");
                          while($namerow = mysqli_fetch_array($resultname)){
                             $id = $namerow['id'];
                             $title = $namerow['title'];
@@ -84,7 +95,7 @@ border-bottom-right-radius: 16px;
                         <i class="fas fa-minus"></i>
                       </button>
 
-                      <input id="form1" min="0" name="quantity" value="0" type="number"
+                      <input id="form1" min="0" name="quantity_food" value="0" type="number"
                         class="form-control form-control-sm" />
 
                       <button class="btn btn-link px-2"
@@ -99,186 +110,9 @@ border-bottom-right-radius: 16px;
                       <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
                     </div>
                   </div>
+
                 <?php } ?>
-                <hr class="my-4">
-                <h3>Chicken Steak</h3>
-                    <?php
-                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id=13");
-                         while($namerow = mysqli_fetch_array($resultname)){
-                            $id = $namerow['id'];
-                            $title = $namerow['title'];
-                            $price = $namerow['price'];
-                            $image_name = $namerow['image_name'];
-                    ?>
                   <hr class="my-4">
-                  
-
-                  <div class="row mb-4 d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
-                      <img
-                        src="images/food/<?php echo $image_name; ?>"
-                        class="img-fluid rounded-3">
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                      <h6 class="text-black mb-0"><?php echo $title; ?></h6>
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                        <i class="fas fa-minus"></i>
-                      </button>
-
-                      <input id="form1" min="0" name="quantity" value="0" type="number"
-                        class="form-control form-control-sm" />
-
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                        <i class="fas fa-plus"></i>
-                      </button>
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                      <h6 class="mb-0">IDR. <?php echo $price; ?></h6>
-                    </div>
-                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                      <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                    </div>
-                  </div>
-                <?php } ?>
-                <hr class="my-4">
-                <h3>Fish Steak</h3>
-                    <?php
-                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id=14");
-                         while($namerow = mysqli_fetch_array($resultname)){
-                            $id = $namerow['id'];
-                            $title = $namerow['title'];
-                            $price = $namerow['price'];
-                            $image_name = $namerow['image_name'];
-                    ?>
-                  <hr class="my-4">
-                  
-
-                  <div class="row mb-4 d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
-                      <img
-                        src="images/food/<?php echo $image_name; ?>"
-                        class="img-fluid rounded-3">
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                      <h6 class="text-black mb-0"><?php echo $title; ?></h6>
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                        <i class="fas fa-minus"></i>
-                      </button>
-
-                      <input id="form1" min="0" name="quantity" value="0" type="number"
-                        class="form-control form-control-sm" />
-
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                        <i class="fas fa-plus"></i>
-                      </button>
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                      <h6 class="mb-0">IDR. <?php echo $price; ?></h6>
-                    </div>
-                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                      <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                    </div>
-                  </div>
-                <?php } ?>
-                <hr class="my-4">
-                <h3>Pork Steak</h3>
-                    <?php
-                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id=15");
-                         while($namerow = mysqli_fetch_array($resultname)){
-                            $id = $namerow['id'];
-                            $title = $namerow['title'];
-                            $price = $namerow['price'];
-                            $image_name = $namerow['image_name'];
-                    ?>
-                  <hr class="my-4">
-                  
-
-                  <div class="row mb-4 d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
-                      <img
-                        src="images/food/<?php echo $image_name; ?>"
-                        class="img-fluid rounded-3">
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                      <h6 class="text-black mb-0"><?php echo $title; ?></h6>
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                        <i class="fas fa-minus"></i>
-                      </button>
-
-                      <input id="form1" min="0" name="quantity" value="0" type="number"
-                        class="form-control form-control-sm" />
-
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                        <i class="fas fa-plus"></i>
-                      </button>
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                      <h6 class="mb-0">IDR. <?php echo $price; ?></h6>
-                    </div>
-                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                      <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                    </div>
-                  </div>
-                <?php } ?>
-                <hr class="my-4">
-                <h3>Tempeh Steak</h3>
-                    <?php
-                         $resultname = mysqli_query($conn, "SELECT * FROM tbl_food WHERE category_id=16");
-                         while($namerow = mysqli_fetch_array($resultname)){
-                            $id = $namerow['id'];
-                            $title = $namerow['title'];
-                            $price = $namerow['price'];
-                            $image_name = $namerow['image_name'];
-                    ?>
-                  <hr class="my-4">
-                  
-
-                  <div class="row mb-4 d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
-                      <img
-                        src="images/food/<?php echo $image_name; ?>"
-                        class="img-fluid rounded-3" alt="Cotton T-shirt">
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
-                      <h6 class="text-black mb-0"><?php echo $title; ?></h6>
-                    </div>
-                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                        <i class="fas fa-minus"></i>
-                      </button>
-
-                      <input id="form1" min="0" name="quantity" value="0" type="number"
-                        class="form-control form-control-sm" />
-
-                      <button class="btn btn-link px-2"
-                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                        <i class="fas fa-plus"></i>
-                      </button>
-                    </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                      <h6 class="mb-0">IDR. <?php echo $price; ?></h6>
-                    </div>
-                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                      <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
-                    </div>
-                  </div>
-                <?php } ?>
-
-                  <hr class="my-4">
-
                   <div class="pt-5">
                     <h6 class="mb-0"><a href="index.php" class="text-body"><i
                           class="fas fa-long-arrow-alt-left me-2"></i>Kembali</a></h6>
@@ -293,7 +127,7 @@ border-bottom-right-radius: 16px;
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                        <label for="">Jumlah orang</label>
+                        <label for="">Jumlah Orang</label>
                       <input name="quantity_people" type="number" id="form3Example1c" class="form-control" placeholder="Jumlah orang" required/>
                       <label for="">Anak diatas 2 tahun, harap dimasukan</label>
                     </div>
@@ -311,7 +145,7 @@ border-bottom-right-radius: 16px;
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                         <label for="">Waktu Pesan</label>
-                      <input name="order_date" type="time" id="form3Example1c" class="form-control" placeholder="" required/>
+                      <input name="order_time" type="time" id="form3Example1c" class="form-control" placeholder="" required/>
                     </div>
                   </div>
 
@@ -326,14 +160,14 @@ border-bottom-right-radius: 16px;
                   <hr class="my-4">
                  
                   <div class="d-flex justify-content-between mb-5">
-                    <h5 class="text-uppercase">Total price</h5>
+                    <h5 class="text-uppercase">Total</h5>
                     <h5>IDR 137.00</h5>
                   </div>
                   <span style="color:red;">Harap diperiksa kembali sebelum memesan!</span>
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"> 
                     <input name="order" type="submit" value="Pesan Sekarang!" class="btn btn-success btn-lg">
                   </div>
-</form>
+                </form>
                 </div>
               </div>
             </div>
